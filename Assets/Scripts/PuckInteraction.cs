cde5e0b97f9f6f6f0b41e042afade75e974686ab
Tauseef Khan <tauseefk@gmail.com>
2017-11-01 10:08:01 -0400

    initial commit

---
+++ Assets/Scripts/PuckInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(LayerMask.NameToLayer("Boundary") == other.gameObject.layer) {
+			Debug.Log("boing!");
+		}
+	}
+}
